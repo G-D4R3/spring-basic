42d659a57b48739730093c44f8475d434d1c615b
dare <gdare1999@gmail.com>
2022-03-02 21:00:48 +0900

    Member domain implement

---
+++ src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,20 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    // 순수한 자바 메소드를 통해 개발 및 테스트
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member memberA = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(memberA);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("findMember = " + findMember.getName());
+        System.out.println("memberA = " + memberA.getName());
+    }
+}
