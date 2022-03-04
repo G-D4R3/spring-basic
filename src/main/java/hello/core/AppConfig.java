35fdb58ab01f06a535d12eed08b615cf2175b45f
dare <gdare1999@gmail.com>
2022-03-04 14:57:58 +0900

    app configuration refactoring

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -15,11 +17,21 @@ public class AppConfig {
 
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository()); // 생성자 주입
+        return new MemberServiceImpl(memberRepository()); // 생성자 주입
+    }
+
+    // new MemoryMemberRepository의 중복이 제거되었다.
+    // 구현체만 변경해주면 된다.
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 
 
