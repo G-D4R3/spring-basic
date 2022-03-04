0269bd1f515fe3e7020bd50a532de5a52ed1fc45
dare <gdare1999@gmail.com>
2022-03-04 15:26:38 +0900

    app configuration refactoring

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -31,7 +31,8 @@ public class AppConfig {
     }
 
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+       // return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 
