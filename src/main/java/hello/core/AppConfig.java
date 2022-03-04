47f7ef54e55d71c7a88a7a4e793a7376082f6535
dare <gdare1999@gmail.com>
2022-03-04 15:49:54 +0900

    app configuration to Spring

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,27 +9,33 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
     // 관심사의 분리
     // 객체를 생성하고 연결하는 역할과 실행하는 역할이 명확히 분리되었다.
 
-
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository()); // 생성자 주입
     }
 
     // new MemoryMemberRepository의 중복이 제거되었다.
     // 구현체만 변경해주면 된다.
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
        // return new FixDiscountPolicy();
         return new RateDiscountPolicy();
