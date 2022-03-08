e5697b677ff3c3c7bc38ce31ad3b1f4207130226
dare <gdare1999@gmail.com>
2022-03-08 15:49:31 +0900

    stateless service at singleton

--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -18,18 +18,21 @@ class StatefulServiceTest {
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
         // ThreadA : A사용자가 10000원 주문
-        statefulService1.order("userA", 10000);
+        int userAPrice = statefulService1.order("userA", 10000);
 
         // ThreadB : B사용자가 20000원 주문
-        statefulService1.order("userB", 20000);
-
-
+        int userBPrice = statefulService1.order("userB", 20000);
+        /**
         // ThreadA : 사용자A 주문 금액 조회
-        int price = statefulService1.getPrice();
-        System.out.println("price = " + price);
+        //int price = statefulService1.getPrice();
+        //System.out.println("price = " + price);
 
+        // 사용자 A의 주문금액은 10000원이 되어야하는데 20000원이라는 결과가 나옴
+        // 스프링빈을 상태공유로 설정했기 때문에
+        //Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+         **/
 
-        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+        Assertions.assertThat(userAPrice).isNotEqualTo(userBPrice);
     }
 
 
