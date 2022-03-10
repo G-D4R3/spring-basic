3dc05d6a219585d4263198e1e5de1798d822e2d0
dare <gdare1999@gmail.com>
2022-03-10 23:35:23 +0900

    request scope with proxy

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -15,18 +15,22 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    //private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody // 화면 구성 없이 문자 그대로 반환
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
 
-        MyLogger myLogger = myLoggerProvider.getObject(); // 이 때 처음 만들어진다
+        System.out.println("myLogger = " + myLogger.getClass());
+
+        //MyLogger myLogger = myLoggerProvider.getObject(); // 이 때 처음 만들어진다
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
         logDemoService.logic("testId");
+
         return "OK";
     }
 
