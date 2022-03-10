50540748ca461ab87fb41952c53da3c94150a365
dare <gdare1999@gmail.com>
2022-03-10 22:39:00 +0900

    request scope with ObjectProvider

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,17 +15,18 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody // 화면 구성 없이 문자 그대로 반환
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
+
+        MyLogger myLogger = myLoggerProvider.getObject(); // 이 때 처음 만들어진다
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
         logDemoService.logic("testId");
-
         return "OK";
     }
 
